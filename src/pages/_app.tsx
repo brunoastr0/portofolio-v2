af7d35c981d2cff6f03426df655ab72868899e2b
brunoastr0 <brunoangelo.dev@gmail.com>
2024-12-30 17:38:47 -0100

    chore:config next-seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
+import { DefaultSeo } from "next-seo";
+import SEO from "../../next-seo.config";
 import "../styles/globals.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
   );
